a399dea82c8a68e75ca8cedd191eb6da11d60d0f
GalZaken <Gal.zaken@gmail.com>
2017-06-10 22:11:39 +0300

    Function getCurrentUser has been fixed

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,18 +1,26 @@
 angular.module('ShiftsManagerApp').controller('homeCtrl', ['$scope', '$http', function ($scope, $http) {
     // GET THE USER SESSION:
     $scope.currentUser = {};
-    var getCurrentUser = function() {
+    getCurrentUser();
+
+    function getCurrentUser() {
         // GET CURRENT USER OBJECT:
         $http.get('users/currentUser').success(function (response) {
-            currentUser = response;
-            console.log($scope.currentUser);
+            $scope.currentUser = response;
         });
-    }();
+    };
 
     $scope.updateAdminComment = function() {
         var adminComment = $scope.adminComment;
         // push adminComment to DB
 
         $scope.displayedComment = adminComment;
+    }
+
+    $scope.isAdmin = function() {
+        if ($scope.currentUser.isAdmin)
+            return true;
+        else
+            return false;
     }
 }]);
